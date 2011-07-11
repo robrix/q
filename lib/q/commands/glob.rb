e79344b451748ce8b31f7fa6202a82d4a630f5d5
Rob Rix <rob.rix@me.com>
2011-07-11 02:39:07 -0400

    Removes the braces from glob pattern interpolation.

--- lib/q/commands/glob.rb
+++ lib/q/commands/glob.rb
@@ -20,7 +20,7 @@ module Q
 		def help(shortcut, terms)
 			short = usage
 			if terms.include?("-v") or terms.include?("--verbose")
-				long = $/ + @patterns.join($/) + $/ + $/ + "(%{0}, %{1}, ... are replaced with the shortcut and terms)"
+				long = $/ + @patterns.join($/) + $/ + $/ + "(%0, %1, ... are replaced with the shortcut and terms)"
 				short + $/ + long
 			else
 				short + $/ + "(use `q help #{shortcut} --verbose` or `q help #{shortcut} -v` for the relevant patterns)"
@@ -30,7 +30,7 @@ module Q
 		protected
 		
 		def interpolate_pattern(pattern, terms)
-			pattern.gsub(/%\{(\d+?)\}/) do |match|
+			pattern.gsub(/%(\d+)/) do |match|
 				terms[$1.to_i]
 			end
 		end
@@ -43,23 +43,23 @@ module Q
 	]
 	DOCSET_CONTENTS_PATH = "/Contents/Resources/Documents/documentation/*/Reference/"
 	PATTERNS = [ # these patterns are matched within <docset>/Contents/Resources/Documents/documentation/*/Reference/
-		"%{0}%{1}/Reference/reference.html", # iPhone/CoreGraphics
-		"%{0}%{1}Ref/index.html", # iPhone CoreFoundation
-		"%{0}%{1}Ref/Reference/reference.html", # CoreFoundation
-		"%{0}%{1}_Class/%{0}%{1}/%{0}%{1}.html", # iPhone/UIKit
-		"%{0}%{1}_Class/%{0}%{1}ClassReference/%{0}%{1}ClassReference.html",
-		"%{0}%{1}_Class/Introduction/Introduction.html",
-		"%{0}%{1}_class/Reference/%{0}%{1}.html",
-		"%{0}%{1}_class/Reference/Reference.html",
-		"%{0}%{1}_ClassRef/Reference/%{0}%{1}.html", # iPhone/OpenGL ES
-		"%{0}%{1}_protocol/Reference/%{0}%{1}.html",
-		"%{0}%{1}_protocol/Reference/Reference.html",
-		"*/*/%{0}%{1}_*/Reference/Reference.html",
-		"*/*/%{0}%{1}_*/Introduction/Introduction.html",
-		"*/*/%{0}%{1}_*/Reference/%{0}%{1}.html",
-		"*/*/%{0}%{1}_*/%{0}%{1}.html",
-		"ManPages/man3/%{0}%{1}.3.html", # man pages, e.g. OpenGL man pages
-		"*/Classes/%{0}%{1}_WebKitAdditions/Reference/Reference.html",
+		"%0%1/Reference/reference.html", # iPhone/CoreGraphics
+		"%0%1Ref/index.html", # iPhone CoreFoundation
+		"%0%1Ref/Reference/reference.html", # CoreFoundation
+		"%0%1_Class/%0%1/%0%1.html", # iPhone/UIKit
+		"%0%1_Class/%0%1ClassReference/%0%1ClassReference.html",
+		"%0%1_Class/Introduction/Introduction.html",
+		"%0%1_class/Reference/%0%1.html",
+		"%0%1_class/Reference/Reference.html",
+		"%0%1_ClassRef/Reference/%0%1.html", # iPhone/OpenGL ES
+		"%0%1_protocol/Reference/%0%1.html",
+		"%0%1_protocol/Reference/Reference.html",
+		"*/*/%0%1_*/Reference/Reference.html",
+		"*/*/%0%1_*/Introduction/Introduction.html",
+		"*/*/%0%1_*/Reference/%0%1.html",
+		"*/*/%0%1_*/%0%1.html",
+		"ManPages/man3/%0%1.3.html", # man pages, e.g. OpenGL man pages
+		"*/Classes/%0%1_WebKitAdditions/Reference/Reference.html",
 	]
 	PATTERN_PATHS = (DOCSET_PATHS.reduce([]){ |memo, docset_path| memo + PATTERNS.collect{ |pattern| docset_path + DOCSET_CONTENTS_PATH + pattern } }).flatten
 	
