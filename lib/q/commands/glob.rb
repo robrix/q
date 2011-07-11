dd8d683f1c52bdb8feed36a899b7d3142a9526ec
Rob Rix <rob.rix@me.com>
2011-07-11 02:55:17 -0400

    Cleans up the patterns quite a bit to use {} globbing for specific strings. This means `q ns responder` once again works as it ought.

--- lib/q/commands/glob.rb
+++ lib/q/commands/glob.rb
@@ -41,29 +41,19 @@ module Q
 		`xcode-select -print-path`.strip + "/Platforms/*.platform/Developer/Documentation/DocSets/*.docset",
 		"/Library/Developer/Documentation/DocSets/*.docset",
 	]
-	DOCSET_CONTENTS_PATH = "/Contents/Resources/Documents/documentation/*/Reference/"
+	DOCSET_CONTENTS_PATH = "/Contents/Resources/Documents/documentation/*/Reference"
 	PATTERNS = [ # these patterns are matched within <docset>/Contents/Resources/Documents/documentation/*/Reference/
-		"%0%1/Reference/reference.html", # iPhone/CoreGraphics
-		"%0%1Ref/index.html", # iPhone CoreFoundation
-		"%0%1Ref/Reference/reference.html", # CoreFoundation
-		"%0%1_Class/%0%1/%0%1.html", # iPhone/UIKit
-		"%0%1_Class/%0%1ClassReference/%0%1ClassReference.html",
-		"%0%1_Class/Introduction/Introduction.html",
-		"%0%1_class/Reference/%0%1.html",
-		"%0%1_class/Reference/Reference.html",
-		"%0%1_ClassRef/Reference/%0%1.html", # iPhone/OpenGL ES
-		"%0%1_protocol/Reference/%0%1.html",
-		"%0%1_protocol/Reference/Reference.html",
-		"*/*/%0%1_*/Reference/Reference.html",
-		"*/*/%0%1_*/Introduction/Introduction.html",
-		"*/*/%0%1_*/Reference/%0%1.html",
-		"*/*/%0%1_*/%0%1.html",
-		"ManPages/man3/%0%1.3.html", # man pages, e.g. OpenGL man pages
-		"*/Classes/%0%1_WebKitAdditions/Reference/Reference.html",
+		"{,/*/*}/%0%1{_Class,_ClassRef,_Protocol,Ref}/Reference/Reference.html",
+		"{,/*/*}/%0%1{_Class,_ClassRef,_Protocol,Ref}/Reference/%0%1.html",
+		"{,/*/*}/%0%1{_Class,_ClassRef,_Protocol,Ref}/Introduction/Introduction.html",
+		"{,/*/*}/%0%1{_Class,_ClassRef,_Protocol,Ref}/%0%1.html",
+		"{,/*/*}/%0%1{_Class,_ClassRef,_Protocol,Ref}/%0%1/%0%1.html",
+		"{,/*/*}/%0%1{_Class,_ClassRef,_Protocol,Ref}/%0%1ClassReference/%0%1ClassReference.html",
+		"/*/Classes/%0%1_WebKitAdditions/Reference/Reference.html",
 	]
 	PATTERN_PATHS = (DOCSET_PATHS.reduce([]){ |memo, docset_path| memo + PATTERNS.collect{ |pattern| docset_path + DOCSET_CONTENTS_PATH + pattern } }).flatten
 	
-	PREFIXES = %w(ca cf cg dom eagl gl glu ib ns qt ui web)
+	PREFIXES = %w(ca cf cg dom eagl ib ns qt ui web)
 	
 	register_command(PREFIXES.collect{ |prefix| prefix.to_sym }, Glob.new(PATTERN_PATHS, "CLASS -- glob through Cocoa documentation for CLASS"))
 end
